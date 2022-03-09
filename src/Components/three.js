a9cd34098c0efefca12fd6e05062de004b895798
John Doe <leehy0782@naver.com>
2022-03-09 22:37:27 +0900

    update css

--- src/Components/three.js
+++ src/Components/three.js
@@ -3,8 +3,11 @@ import style from '../CSS/three.module.css'
 function Three() {
     return(
         <section id={`${style.three}`}>
-            <div className={`${style.threeImg}`}></div>
-            <div className={`${style.imgCover}`}></div>
+            <div className={`${style.topBtn}`}></div>
+            <div className={`${style.wrap}`}>
+                <div className={`${style.threeImg}`}></div>
+                <div className={`${style.imgCover}`}></div>
+            </div>
         </section>
     )
 }
