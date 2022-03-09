36da54c6ff2a9525980e644b316afc64131e0b15
John Doe <leehy0782@naver.com>
2022-03-09 18:55:21 +0900

    create second.js and second.module.css

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './CSS/reset.css';
 import './CSS/style.css';
 import Header from './Components/header';
 import First from './Components/first';
+import Second from './Components/second';
 
 function App() {
   return (
     <>
-    {/* <Header/> */}
-    <First/>
+    {/* <Header/>
+    <First/> */}
+    <Second/>
     </>
   );
 }
---
+++ src/Components/second.js
@@ -0,0 +1,11 @@
+import style from '../CSS/second.module.css'
+
+function Second() {
+    return(
+        <>
+            <section id={`${style.second}`}></section>
+        </>
+    ) 
+}
+
+export default Second;
