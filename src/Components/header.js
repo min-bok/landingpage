6affb9fb48a6248282a8d7b519f23664f04c5c1e
John Doe <leehy0782@naver.com>
2022-03-09 17:33:48 +0900

    update header.module.css

--- src/Components/header.js
+++ src/Components/header.js
@@ -5,9 +5,11 @@ function Header() {
         <section id={`${style.header}`}>
             <div className={`${style.logo}`}></div>
             <div className={`${style.menu}`}>
-                <p className={`${style.home}`}>Home</p>
-                <p className={`${style.about}`}>About</p>
-                <p className={`${style.support}`}>Support</p>
+                <div className={`${style.wrap}`}>
+                    <p className={`${style.home}`}>Home</p>
+                    <p className={`${style.about}`}>About</p>
+                    <p className={`${style.support}`}>Support</p>
+                </div>
                 <button className={`${style.download}`}>Download</button>
             </div>
         </section>
