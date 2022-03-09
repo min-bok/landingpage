ff7ea47d749cef58f1cfc0cf0b3556e3abdf76be
John Doe <leehy0782@naver.com>
2022-03-09 20:18:50 +0900

    update second.js one

--- src/Components/second.js
+++ src/Components/second.js
@@ -3,7 +3,24 @@ import style from '../CSS/second.module.css'
 function Second() {
     return(
         <>
-            <section id={`${style.second}`}></section>
+            <section id={`${style.second}`}>
+                <div className={`${style.one}`}>
+                    <div className={`${style.oneImg}`}></div>
+                    <div className={`${style.oneWrap}`}> 
+                        <p className={`${style.text}`}>
+                            Lorem Ipsum is simply<br/>
+                            dummy text of the printing and dummy text
+                        </p>
+                        <p className={`${style.textDetail}`}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting<br/>
+                        industry. 
+                        </p>
+                    </div>
+                </div>
+                <div className={`${style.two}`}></div>
+                <div className={`${style.three}`}></div>
+                <div className={`${style.four}`}></div>    
+            </section>
         </>
     ) 
 }
