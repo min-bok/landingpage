c30871bd75d57199ea98545fcc927163e6ffdc82
John Doe <leehy0782@naver.com>
2022-03-09 22:09:41 +0900

    update css

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -2,7 +2,15 @@ import style from '../CSS/footer.module.css'
 
 function Footer() {
     return(
-        <section id={`${style.footer}`}></section>
+        <section id={`${style.footer}`}>
+            <div className={`${style.logo}`}></div>
+            <div className={`${style.wrap}`}>
+                <div className={`${style.blog}`}></div>
+                <div className={`${style.insta}`}></div>
+                <div className={`${style.facebook}`}></div>
+                <div className={`${style.youtube}`}></div>
+            </div>
+        </section>
     )
 }
 
