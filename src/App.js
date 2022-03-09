da1e3acb7e5c7419f96db6cf26904af814df7b81
John Doe <leehy0782@naver.com>
2022-03-09 17:41:54 +0900

    create firstPage

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './CSS/reset.css';
 import './CSS/style.css';
 import Header from './Components/header';
+import First from './Components/firstPage';
 
 function App() {
   return (
     <>
     <Header/>
+    <First/>
     </>
   );
 }
---
+++ src/Components/firstPage.js
@@ -0,0 +1,8 @@
+function First() {
+    return(
+        <>
+        </>
+    )
+}
+
+export default First; 
