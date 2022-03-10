cd90446496d53a260486e7cd44ba1a59d48f7d8b
John Doe <leehy0782@naver.com>
2022-03-11 01:08:06 +0900

    update first.module.css

--- src/Components/first.js
+++ src/Components/first.js
@@ -6,14 +6,23 @@ function First() {
             <div className={`${style.wrap}`}>
                 <div className={`${style.textWrap}`}>
                     <p className={`${style.text}`}>
-                        <span className={`${style.textWebOne}`}>Lorem Ipsum is simply</span>
-                        <span className={`${style.textMobileOne}`}>dummy text</span> of the printing and
+                        <span className={`${style.textWeb}`}>Lorem Ipsum is simply</span>
+                        <span className={`${style.textMobile}`}>dummy text</span> of the printing and
                     </p>
                     <p className={`${style.detailText}`}>
+                    <span className={`${style.webText}`}>
                     Lorem Ipsum is simply dummy text of the printing and<br/>
                     typesetting industry.<br/>
                     Lorem Ipsum has been the industry's standard dummy text<br/>
-                    ever since the 1500s, when an unknown    
+                    ever since the 1500s, when an unknown
+                    </span>
+                    <span className={`${style.mobileText}`}>
+                    Lorem Ipsum is simply dummy text<br/>
+                    of the printing and typesetting industry.<br/>
+                    Lorem Ipsum has been the industry's<br/>
+                    standard dummy text ever since the 1500s,<br/>
+                    when an unknown
+                    </span>
                     </p>
                     <button className={`${style.btn}`}>Download</button>
                 </div>
