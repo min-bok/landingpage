11e2e4d6873681ae9b572202ad90ac97d9a2a987
John Doe <leehy0782@naver.com>
2022-03-13 00:09:20 +0900

    update footer.module.css

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -10,6 +10,12 @@ function Footer() {
                 <div className={`${style.facebook}`}></div>
                 <div className={`${style.youtube}`}></div>
             </div>
+            <ul className={`${style.moblie}`}>
+                <li className={`${style.menu}`}>About</li>
+                <li className={`${style.menu}`}>Blog</li>
+                <li className={`${style.menu}`}>Support</li>
+                <li className={`${style.menu}`}>Terms of Use</li>
+            </ul>
         </section>
     )
 }
