6f00cf9486e3069508ad0edc8daf40269767bce1
John Doe <leehy0782@naver.com>
2022-03-14 01:49:49 +0900

    update sub.js and sub.module.css for web size

--- src/Components/subscribe.js
+++ src/Components/subscribe.js
@@ -11,17 +11,19 @@ function Subscribe() {
     return(
         <section id={isShow ? `${style.modal}` : `${style.subscribe}`}>
             <div className={isShow ? `${style.hideSub}` : `${style.showSub}`}>
-                <div className={`${style.textWrap}`}>
-                    <p className={`${style.title}`}>Subscribe to our Blog post</p>
-                    <p className={`${style.text}`}>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                        Lorem Ipsum has been the industry's standard dummy text
-                    </p>
-                </div>
-                <div className={`${style.searchBar}`}>
-                    <div className={`${style.icon}`}></div>
-                    <input type="text" className={`${style.input}`} placeholder='Enter your e-mail address'/>
-                    <button onClick={showModal} className={`${style.btn}`}>Subscribe</button>
+                <div className={`${style.subCont}`}>
+                    <div className={`${style.textWrap}`}>
+                        <p className={`${style.title}`}>Subscribe to our Blog post</p>
+                        <p className={`${style.text}`}>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                            Lorem Ipsum has been the industry's standard dummy text
+                        </p>
+                    </div>
+                    <div className={`${style.searchBar}`}>
+                        <div className={`${style.icon}`}></div>
+                        <input type="text" className={`${style.input}`} placeholder='Enter your e-mail address'/>
+                        <button onClick={showModal} className={`${style.btn}`}>Subscribe</button>
+                    </div>
                 </div>
             </div>
 
